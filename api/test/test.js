23c7bff76afef77e2f16e2df036a2d2a8a55185a
jdarosaj <jdarosaj@gmail.com>
2019-04-19 18:25:10 -0300

    Tests less numbers

--- api/test/test.js
+++ api/test/test.js
@@ -2,8 +2,8 @@ const controller = require('../controllers/porExtensoController');
 
 function startTest(){
 
-    const startInterval = -1500;
-    const stopInterval = 1500;
+    const startInterval = -30;
+    const stopInterval = 102;
 
     for(let i = startInterval;i <= stopInterval; i++) {
         console.log(`i=${i}  fullNumber=${controller.fullNumber(i)}`);
@@ -13,6 +13,7 @@ function startTest(){
     console.log(`i=-99999  fullNumber=${controller.fullNumber(-99999)}`);
     console.log(`i=50000  fullNumber=${controller.fullNumber(50000)}`);
     console.log(`i=500  fullNumber=${controller.fullNumber(500)}`);
+    console.log(`i=58664  fullNumber=${controller.fullNumber(58664)}`);
 }
 
 startTest();
